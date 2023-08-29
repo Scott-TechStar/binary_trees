220059fdf52fcc53604964fc5c2ad1908077e348
syphrose <swabulwenyi@gmail.com>
2023-08-29 15:02:37 -0700

    Depth of a node in a B.tree

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - the depth of a node in a tree
+ * @tree: ptr to the node to measure the depth
+ *
+ * Return: 0 If tree is NULL
+ *
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	if (!tree)
+		return (0);
+
+	while (tree->parent)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+
+	return (depth);
+}
+
