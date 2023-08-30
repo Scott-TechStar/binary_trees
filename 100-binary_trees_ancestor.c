a7e579ad1a7f2b9fc2028e3b0d490c76f1bba5ff
John Mwadime <johnniekips@gmail.com>
2023-08-30 18:54:46 +0300

    Update 100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -7,35 +7,20 @@
  *
  * Return: If no common ancestors return NULL, else return common ancestor.
  */
-/* Function to find the lowest common ancestor */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second) {
-    if (first == NULL || second == NULL)
-        return NULL;
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
+{
+	binary_tree_t *mom, *pop;
 
-    // Create a set to store ancestors of the first node
-    binary_tree_t *ancestors[100];  // Assuming the maximum tree depth is 100
-    size_t num_ancestors = 0;
+	if (!first || !second)
+		return (NULL);
+	if (first == second)
+		return ((binary_tree_t *)first);
 
-    // Store ancestors of the first node in the set
-    const binary_tree_t *current = first;
-    while (current != NULL) {
-        ancestors[num_ancestors] = (binary_tree_t *)current;
-        num_ancestors++;
-        current = current->parent;
-    }
-
-    // Check ancestors of the second node
-    current = second;
-    while (current != NULL) {
-        // If we find a common ancestor, return it
-        for (size_t i = 0; i < num_ancestors; i++) {
-            if (current == ancestors[i]) {
-                return (binary_tree_t *)current;
-            }
-        }
-        current = current->parent;
-    }
-
-    // No common ancestor found
-    return NULL;
+	mom = first->parent, pop = second->parent;
+	if (first == pop || !mom || (!mom->parent && pop))
+		return (binary_trees_ancestor(first, pop));
+	else if (mom == second || !pop || (!pop->parent && mom))
+		return (binary_trees_ancestor(mom, second));
+	return (binary_trees_ancestor(mom, pop));
 }
